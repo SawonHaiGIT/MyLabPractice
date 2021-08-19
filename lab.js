8175c0c089a8affba91caf67db879742870ea193
Sawon Haither (eHealth NSW) <Sawon.Haither@health.nsw.gov.au>
2021-08-19 12:19:28 +1000

    changed lab.js

---
+++ lab.js
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>My Lab</title>
+        </head>
+        <body>
+            This is my test Lab
+            </body>
+            </html>
+            console.log('Hello');
